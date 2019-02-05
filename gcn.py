ded8f2e92f50b56d309f1d9649953b0c2c481a10
teja5832 <teja.cool.rocks@gmail.com>
2019-02-05 14:13:14 -0600

    'More progress'

--- gcn.py
+++ gcn.py
@@ -1,9 +1,7 @@
 '''
     GCN Implementation for our purposes.
     Based on 
-    https://stackoverflow.com/questions/36427219/can-i-write-a-custom-layer-in-tensorflow-in-python-with-existing-ops-such-as-con
-    and
-    https://github.com/tkipf/gcn
+        https://github.com/tkipf/gcn
 '''    
 
 import tensorflow as tf
@@ -17,9 +15,28 @@ def construct_gcn_features(curr_flow, adj_mat, flows, visited, isolation):
         elif j == curr_flow:
             for q in visited[i]:
                 curr_visited.add(q)
-        
 
 def gcn(adj_mat, features=None):
+    '''https://stackoverflow.com/questions/36427219/'''
     kernel = tf.Variable()
     bias = tf.Variable()
     if features is None:
+        pass
+
+class GCN(tf.keras.layers.Layer):
+
+    def __init__(self, num_outputs):
+        super(gcn, self).__init__()
+        self.num_inputs = -1
+        self.num_outputs = num_outputs
+
+        self.kernel = None
+        self.bias = None
+
+    def build(self, input_shape):
+        self.num_inputs = int(input_shape[-1])
+        self.kernel = tf.Variable("kernel", shape=(self.num_inputs, self.num_outputs))
+        self.bias = tf.Variable("bias", shape=(self.num_outputs,))
+
+    def call(self, adj_mat, features=None):
+        pass
